3ff3dd2412a2b985ed56fbd0ec45c2e7ac6f4e9f
Ishii-Nozomi <xx2468.egg@gmail.com>
2023-01-28 12:18:31 +0900

    ①を追加

--- Contents.swift
+++ Contents.swift
@@ -1,2 +1,15 @@
 import UIKit
 
+class AirCleaner {
+    // ①運転する
+    func startButton(pushButton: Bool) {
+        if pushButton == true {
+            print("運転する")
+        } else {
+            print("停止中")
+        }
+    }
+    
+}
+
+
