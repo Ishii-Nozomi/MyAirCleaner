e309928bc9b280de42c5f1d9737b50a8199ccdd7
Ishii-Nozomi <xx2468.egg@gmail.com>
2023-01-31 16:49:40 +0900

    ②、③を追加

--- Contents.swift
+++ Contents.swift
@@ -1,15 +1,58 @@
 import UIKit
+    
+// ①運転する.停止ボタン
+private func startButton(pushStartButton: Bool) -> Void {
+    if pushStartButton == true {
+        modes.append(.normal)
+        print("通常モードスタート")
+    } else {
+        print("停止中")
+    }
+}
 
-class AirCleaner: UIViewController {
-    // ①運転する
-    func startButton(pushButton: Bool) {
-        if pushButton == true {
-            print("通常モード運転")
-        } else {
-            print("停止中")
+    // ②モードを選択できる
+    enum Mode: String, CaseIterable {
+    case normal = "通常モード"
+    case humidification = "加湿モード"
+    case blowingAir = "送風モード"
+    case circulation = "循環モード"
+    }
+    var modes: [Mode] = [.normal, .humidification, .blowingAir, .circulation]
+    
+private func selectMode(pushModeButton: Mode) {
+    switch pushModeButton {
+    case .normal :
+        print("通常モード")
+    case .humidification:
+        print("加湿モード")
+    case .blowingAir:
+        print("送風モード")
+    case .circulation:
+        print("循環モード")
+    }
+}
+
+    // ③風量調整できる
+    enum AirFlow: String, CaseIterable {
+        case delicate = "弱"
+        case medium = "中"
+        case powerful = "強"
+    }
+    var airFlows: [AirFlow] = [.delicate, .medium, .powerful]
+    
+    private func strengthButton(pushStrengthButton: AirFlow) {
+        switch pushStrengthButton {
+        case .delicate :
+            print("弱")
+        case .medium :
+            print("中")
+        case .powerful :
+            print("強")
         }
     }
-    
-}
+startButton(pushStartButton: true)
+selectMode(pushModeButton: .blowingAir)
+strengthButton(pushStrengthButton: .powerful)
+
 
 
